7e50c9403adba2b49e26e348bc2e1fd98d512d6f
Jeremy Josol <jeremyjosol@gmail.com>
2023-11-13 11:31:47 -0800

    Add User bio section + basic styles

--- src/components/User.js
+++ src/components/User.js
@@ -17,7 +17,9 @@ function User() {
           <li className="info-followers"><a href="/followers" className="info-link">FOLLOWERS</a></li>
         </ul>
       </div>
-      
+      <div className="user-bio">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
     </React.Fragment>
   );
 }
