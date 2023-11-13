ed59e46a30a73ac6e2e1ec721a742e60b1f71cd5
Jeremy Josol <jeremyjosol@gmail.com>
2023-11-13 10:38:04 -0800

    Add User header section + img

--- src/components/User.js
+++ src/components/User.js
@@ -1,9 +1,14 @@
 import React from "react";
+import profilePicture from "./../img/pfp.jpg";
 
 function User() {
   return (
     <React.Fragment>
       <div className="user">
+      <div className="user-header">
+          <img src={profilePicture} alt="PFP" className="profile-picture" />
+          <h2 className="username">Jeremy Josol</h2>
+        </div>
         <ul className="info">
           <li className="info-posts"><a href="/" className="info-link">POSTS</a></li>
           <li className="info-following"><a href="/following" className="info-link">FOLLOWING</a></li>
